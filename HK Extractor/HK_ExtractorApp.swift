2873550f4eeba7173796d890ce0ce45907a74987
thomaah <thomaah@outlook.com>
2026-03-01 12:15:11 +0000

    Initial Commit

---
+++ HK Extractor/HK_ExtractorApp.swift
@@ -0,0 +1,17 @@
+//
+//  HK_ExtractorApp.swift
+//  HK Extractor
+//
+//  Created by Tom Howell on 01/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct HK_ExtractorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
